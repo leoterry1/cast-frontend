1e29f96a8b63922145292ed1266c98496a8e97c7
Lucas Enriquez <lucas.enriquez.leonel@gmail.com>
2024-05-30 23:32:41 -0300

    feat: add  twitch panel

---
+++ src/components/twitch-live.tsx
@@ -0,0 +1,26 @@
+const TwitchLive = () => {
+  return (
+    <div className="h-screen">
+      <h1 className="font-castFont font-semibold text-2xl my-2 text-castTitleDisabled">
+        VIVO
+      </h1>
+      <div className="h-full flex">
+        <div className="relative overflow-hidden w-3/4 h-5/6 mr-2">
+          <iframe
+            src="https://player.twitch.tv/?channel=nanu_0_&parent=localhost"
+            allowFullScreen
+            className="w-full h-full"
+          ></iframe>
+        </div>
+        <div className="relative overflow-hidden w-1/4 h-5/6">
+          <iframe
+            src="https://www.twitch.tv/embed/nanu_0_/chat?parent=localhost"
+            className="w-full h-full"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TwitchLive;
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,9 @@
+import TwitchLive from "../components/twitch-live";
 
 export const HomePage = () => {
     return (
-        <div>
+        <div className="w-full h-full px-10">
+            <TwitchLive/>
         </div>
     )
 };
